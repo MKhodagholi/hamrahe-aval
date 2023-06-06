a489737155f4b8b665d45615471491e4808ef501
MKhodagholi <mdkhodagholi@gmail.com>
2023-06-06 10:58:34 +0330

    code: initial Store for redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+
+import store from "./store";
 
 // replace console.* for disable log on production
 if (process.env.NODE_ENV === "production") {
@@ -14,6 +17,6 @@ const app = document.getElementById("app");
 
 const root = createRoot(app);
 
-root.render(<App />);
+root.render(<Provider store={store}></Provider>);
 
 reportWebVitals();
